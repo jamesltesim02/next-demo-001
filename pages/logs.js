bbddf299fc6b60b0e828b94baec529a0f67c43a0
connor <connor@nbmm.co>
2019-11-16 21:33:37 +0800

    不知道改了啥

--- pages/logs.js
+++ pages/logs.js
@@ -3,7 +3,7 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 
-import { dateFormat } from '../utils/common-utils'
+import { dateFormat } from '../../utils/common-utils'
 
 import Layout from '../components/common/layout'
 import { withBaseApi } from '../api/base'
